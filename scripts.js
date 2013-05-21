ccb19f1cf660386536b8f1978b88ff496e780990
Kelsey Lawrence <kelseylawrence@Kelseys-MacBook-Pro.local>
2013-05-21 15:45:14 -0700

    Resourcefulness implemented ;)
    Even to odd.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 // it worked 
 
 $(document).ready(function(){
-$("li:even > h2").text("<-- Check it out!");
+$("#vacations > li:odd").text("<-- Check it out!");
 });
 
 //it worked again! I can't believe it!
